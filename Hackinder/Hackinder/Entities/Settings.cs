0cca3f16d83c5b2e560804fc9da64e8e17c195be
Andrey Alexandrow <andrey.alexandrow@uptick.com>
2017-10-21 21:44:51 +0300

    фичакат

--- Hackinder/Hackinder/Entities/Settings.cs
+++ Hackinder/Hackinder/Entities/Settings.cs
@@ -6,8 +6,5 @@ namespace Hackinder.Entities
     public class Settings
     {
         public List<string> DesiredSkills { get; set; }
-        public List<string> DesiredSpecializations { get; set; }
-        public DateTime FromAge { get; set; }
-        public DateTime ToAge { get; set; }
     }
 }
